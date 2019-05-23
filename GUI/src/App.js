d7f739a95b8afa3ec8da12ab0db2d5a1a39e6bce
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-05-24 01:45:17 +0200

    Fixing Sign In
    Sign In Fixed

--- GUI/src/App.js
+++ GUI/src/App.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard'
-import InputBar from "./InputBar";
 import './App.css';
 
 class App extends React.Component{
-
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            submitted: false,
+        };
+
+        // Bind 'this' to event handlers. React ES6 does not do this by default
+        this.submittedCheck = this.submittedCheck.bind(this);
+        this.onUsernameChange = this.onUsernameChange.bind(this);
     }
 
+    submittedCheck = () => {
+        this.setState({submitted: true})
+    };
+
+    onUsernameChange = (input) => {
+        this.setState({username: input})
+    };
 
     render() {
-        if (this.props.submitted) {
+        if (this.state.submitted) {
             // Form was submitted, now show the main App
             return (
-                <Dashboard username={this.props.username} />
+                <Dashboard username={this.state.username} />
             );
         }
 
         // Initial page load, show a simple login form
         return (
-            <SignIn />
+            <SignIn submittedCheck={this.submittedCheck} onUsernameChange={this.onUsernameChange}/>
         );
     }
-
-
-    //return (
-    //<div className="App">
-    //    <Dashboard />
-    //    <SignIn />
-    //</div>
-  //);
 }
-
-App.defaultProps = {
-    username: ''
-};
 
 export default App;
 
