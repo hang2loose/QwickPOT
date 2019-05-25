16f8d4158c4f46e1d7b9567afeccfa40913054b2
hang2loose <hang2loose@hotmail.de>
2019-05-25 22:43:22 +0200

    fixed swagger issues :D

--- data-service/src/main/java/qwickpot/dataservice/config/SwaggerConfig.java
+++ data-service/src/main/java/qwickpot/dataservice/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
 package qwickpot.dataservice.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -11,18 +11,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
   @Bean
-  public Docket productApi() {
+  public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
-        .select()                 .apis(RequestHandlerSelectors.basePackage("qwickpot.dataservice.controllers"))
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("qwickpot.dataservice.controllers"))
+        .paths(PathSelectors.any())
         .build();
-  }
-  @Override
-  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
-    registry.addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 }
--- data-service/src/main/java/qwickpot/dataservice/controllers/CardController.java
+++ data-service/src/main/java/qwickpot/dataservice/controllers/CardController.java
@@ -1,13 +1,13 @@
 package qwickpot.dataservice.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CardController {
 
-  @RequestMapping("/dummy")
+  @GetMapping("/dummy")
   public String dummyEndPoint() {
     return "Hello World";
   }
