eb94a020d76cb66200e209e12eda04842a63a0f2
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-05-26 16:24:40 +0200

    Update Message.js

--- GUI/src/Message.js
+++ GUI/src/Message.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+
+const styles = theme => ({
+    card: {
+        maxWidth: 400,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: 'red',
+    },
+});
 
 class Message extends React.Component {
     render() {
+
+        const { classes } = this.props;
+
         return (
             <Card>
-                <div>
-                    { this.props.username }
-                </div>
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label={this.props.username} className={classes.avatar}>
+                            {this.props.username[0]}
+                        </Avatar>
+                    }
+                    subheader={this.props.username}
+                />
                 <div>
                     { this.props.message }
                 </div>
@@ -21,4 +56,4 @@ Message.defaultProps = {
     username: '',
 };
 
-export default Message;
+export default withStyles(styles)(Message);
