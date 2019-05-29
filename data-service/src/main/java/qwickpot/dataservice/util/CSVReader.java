a818ff6bb5edcafe0f55472349349c0510b37e81
StefanTobert <47445366+StefanTobert@users.noreply.github.com>
2019-05-29 19:42:35 +0200

    try with resources
    try with resources for BufferedReader

--- data-service/src/main/java/qwickpot/dataservice/util/CSVReader.java
+++ data-service/src/main/java/qwickpot/dataservice/util/CSVReader.java
@@ -1,7 +1,6 @@
 package qwickpot.dataservice.util;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -11,29 +10,17 @@ public class CSVReader {
   public static final int CSV_MIN_COLUMN_COUNT = 3;
 
   public static LinkedList<String[]> readCSV(String csvFile) {
-    BufferedReader br = null;
     String line = "";
     LinkedList<String[]> result = new LinkedList<>();
-    try {
-      br = new BufferedReader(new FileReader(csvFile));
+    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
       while ((line = br.readLine()) != null) {
         String[] entryArray = line.split(CSV_DELIMITER);
         if (entryArray.length > CSV_MIN_COLUMN_COUNT - 1) {
           result.add(entryArray);
         }
       }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
-    } finally {
-      if (br != null) {
-        try {
-          br.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
     }
     return result;
   }
