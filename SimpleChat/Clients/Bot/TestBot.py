d13547e9bc9b95ad60f688ea4695b11920c6e681
StefanTobert <47445366+StefanTobert@users.noreply.github.com>
2019-06-17 22:01:01 +0200

    Added id to eventbuilder

--- SimpleChat/Clients/Bot/TestBot.py
+++ SimpleChat/Clients/Bot/TestBot.py
@@ -2,13 +2,11 @@ from Clients.Bot.Qwickpot import Qwickpot
 
 
 class TestBot:
-    bot = Qwickpot("q")
-
     def test_bot(self, bot):
         msg = {'event_type': 'new_user',
                'load': {'username': 'rggr'},
                'ID': '7c4eca676b884610b5e2321bc2045889'}
-        print(bot.trigger_bot(msg))
+        bot.trigger_bot(msg)
         question = ""
         while "kill" != question:
             question = input("-> ")
@@ -17,4 +15,8 @@ class TestBot:
                 "load": {"username": "chucky", "question": question},
                 "ID": '7c4eca676b884610b5e2321bc2045889'
             }
-            print(bot.trigger_bot(msg))
+            bot.trigger_bot(msg)
+
+
+bot = Qwickpot("q")
+TestBot().test_bot(bot)
