70a9303ef3321179dceb2caf59e118751a9a8653
Jularobbe <jularobbe@web.de>
2019-06-25 14:15:45 +0200

    Message sent when user disconnects, added message structure json
    Example:
    
    Sid is sent in following format to signal user_disconnect:
    
    Sent: {
    'ID': '5db81991e30b4089b35efac9d2c26e1e',
    'event_type': 'user_disconnected'
    }

--- SimpleChat/Server/EventHandler.py
+++ SimpleChat/Server/EventHandler.py
@@ -4,6 +4,16 @@ import socketio
 class EventHandler:
     sio = socketio.Server()
 
+    @sio.event
+    def disconnect(self):
+        disconnect_message = {
+            "ID": self,
+            "event_type": "user_disconnected"
+        }
+
+        EventHandler.sio.emit('user_disconnected', disconnect_message)
+        print("Sent: " + str(disconnect_message))
+
     @sio.on('user_send')
     def echo_message(sid, message):
         print('Received event: {} from user: {}'.format(message, sid))
