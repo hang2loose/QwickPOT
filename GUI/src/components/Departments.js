e8e3b24b66572c1c0477d83b133550d35c254739
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-06-25 10:31:59 +0200

    Update Departments.js
    Direct api link changed to dependency on config.json

--- GUI/src/components/Departments.js
+++ GUI/src/components/Departments.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Config from '../config/config.json'
 
-const API = 'http://localhost:9090/getAllDepartmentNames';
+const API = Config["data-service"].api;
 
 class Departments extends React.Component {
 
