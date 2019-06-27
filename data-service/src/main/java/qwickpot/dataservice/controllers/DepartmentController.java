4af4052fa823138c9ee2b623e5e38ef0deb9c369
hang2loose <hang2loose@hotmail.de>
2019-06-27 08:49:55 +0200

    added DepartmentController on domain

--- data-service/src/main/java/qwickpot/dataservice/controllers/DepartmentController.java
+++ data-service/src/main/java/qwickpot/dataservice/controllers/DepartmentController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import qwickpot.dataservice.domain.Deparment;
 import qwickpot.dataservice.services.DeparmentService;
 
 @RestController
+@RequestMapping("/DepartmentController")
 public class DepartmentController {
 
   private DeparmentService deparmentService;
